709b90e30ee17ce1860e232836518d424040b2df
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-07-23 16:53:27 +0100

    module 01

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -20,7 +20,7 @@ int main(int ac, char **av)
 	std::string	data;
 	char	*fdata;
 	char	*temp;
-	size_t		found=0;
+	std::size_t		found=0, l;
 	int			i=0, j=0, x=0;
 
 	
@@ -34,35 +34,45 @@ int main(int ac, char **av)
 			found = data.find(av[2]);
 			if (found != std::string::npos)
 			{
-				while ((i < found) & (data[i] != '\0'))
-				{
-					fdata[i] = data[i];
-					i++;
-				}
-				x = i+strlen(av[2]);
-				while (av[3][j] != '\0')
-				{
-					fdata[i] = av[3][j];
-					i++;
-					j++;
-				}
-				while (j < strlen(av[2]))
-				{
-					i++;
-					j++;
-				}
-				fdata[i] = '\0';
-				i = 0;
-				temp = strdup("");
-				while (x < data.length())
-				{
-					temp[i] = data[x];
-					i++;
-					x++;
-				}
-				temp[i] = '\0';
-				fdata = strcat(fdata, temp);
-				newfile << fdata << std::endl;
+				// while ((i < found) & (data[i] != '\0'))
+				// {
+				// 	fdata[i] = data[i];
+				// 	i++;
+				// }
+				l = data.copy(fdata, found, 0);
+				fdata[l] = '\0';
+				x = found+strlen(av[2]);
+				// while (av[3][j] != '\0')
+				// {
+				// 	fdata[i] = av[3][j];
+				// 	i++;
+				// 	j++;
+				// }
+				std::string temp1 = std::string(fdata);
+				temp1.append(std::string(av[3]));
+				// fdata[std::string(fdata).length()] = '\0';
+				// l = std::string(av[3]).copy(fdata, std::string(av[3]).length(), 0);
+				// fdata[l] = '\0';
+				// while (j < strlen(av[2]))
+				// {
+				// 	i++;
+				// 	j++;
+				// }
+				// fdata[i] = '\0';
+				// i = 0;
+				// temp = strdup("");
+				// while (x < data.length())
+				// {
+				// 	temp[i] = data[x];
+				// 	i++;
+				// 	x++;
+				// }
+				// std::string temp2 = std::string(fdata);
+				temp1.append(data, x, data.length()-x);
+				// std::cout << temp1 << '-' << x << std::endl;
+				// temp[i] = '\0';
+				// fdata = strcat(fdata, temp);
+				newfile << temp1 << std::endl;
 			}
 			else
 				newfile << data << std::endl;
